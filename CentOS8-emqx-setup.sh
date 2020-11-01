4cfb6009638ab0f77dd8cc09d4d1e58cede204b7
Bryan Ji <xiaoxi.ji@resideo.com>
2020-11-01 23:11:39 +0800

    Add EMQ X dashboard access comments

--- CentOS8-emqx-setup.sh
+++ CentOS8-emqx-setup.sh
@@ -7,3 +7,5 @@ docker pull emqx/emqx:4.2.2
 docker run -d --name emqx -p 1883:1883 -p 8083:8083 -p 8883:8883 -p 8084:8084 -p 18083:18083 emqx/emqx:4.2.2
 
 #as for Ali could system, need to enable this port in security configuration
+
+#当 EMQ X 成功运行在你的本地计算机上且 EMQ X Dashboard 被默认启用时，你可以访问 http://localhost:18083 来查看你的 Dashboard，默认用户名是 admin，密码是 public
