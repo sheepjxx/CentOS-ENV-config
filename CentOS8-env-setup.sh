7d630ac234663365b076c95ba01613df4d0f0a97
Bryan Ji <xiaoxi.ji@resideo.com>
2020-11-01 22:24:14 +0800

    Add git and docker installation

---
+++ CentOS8-env-setup.sh
@@ -0,0 +1,28 @@
+#install git
+yum -y install git
+
+#install docker
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+sudo yum install -y yum-utils
+
+ sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install docker-ce docker-ce-cli containerd.io
+
+sudo yum install docker-ce docker-ce-cli containerd.io
+
+#start docker deamon
+#sudo systemctl start docker
+
+
+
